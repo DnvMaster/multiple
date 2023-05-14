dfb543eedcf839f88970a450ed27220777698a40
DnvMaster <dnvmaster@yandex.ru>
2023-05-14 17:25:48 +0300

    Create Controller,model,database-factory,seeder & database-migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,18 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        Admin::factory()->create();
+        //
     }
 }
